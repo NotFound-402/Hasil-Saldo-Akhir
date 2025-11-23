0234076afd1faa69baf28fe7f2e7879db94849b4
NotFound-402 <jeezstok2@gmail.com>
2025-11-23 14:17:15 +0700

    Add initial HTML and JavaScript files for saldo calculation

---
+++ script.js
@@ -0,0 +1,5 @@
+let saldoAwal =  Number(prompt("Masukkan Jumlah Saldo Awal Rp."))
+let saldoTambahan = Number(prompt("Masukkan Jumlah Saldo Awal Rp."))
+let saldoAkhir = saldoAwal + saldoTambahan
+
+alert(`Total Saldo Anda Adalah Rp.${saldoAkhir}`)
